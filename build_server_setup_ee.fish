490069a4e63291a7317502a4bee7c4dca3da4ca1
Scott Harwell <scott@harwell.me>
2023-04-10 09:11:59 -0400

    Add build server setup ee function

---
+++ build_server_setup_ee.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/env fish
+
+# Place this file in ~/.config/fish/functions/
+# Then, the `arm_ttk_upgrade` functions will be available from your shell.
+
+function build_server_setup_ee
+    # Run the abc function
+    abc
+    
+    # CD into the context folder
+    cd ./context
+    
+    # Build the container
+    docker buildx build --no-cache --platform linux/arm64 -t scottharwell/server-setup-ee:latest . --push
+    
+    # CD back to the parent folder
+    cd .
+end
